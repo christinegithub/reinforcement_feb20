e864c313f4b8dbe183a39b6a004a885396921cbd
christinegithub <christine13@gmail.com>
2019-02-19 20:47:46 -0500

    exercise 2 complete

---
+++ exercise2.py
@@ -0,0 +1,17 @@
+# Pick three names and store them in a list.
+
+# Prompt the user to enter their name.
+# If their name is one of the names in the list, display a greeting message that includes their name.
+# If their name isn't in the list, display "Who goes there?"
+
+def name_match(user_name, name_list):
+    for name in name_list:
+        if name == user_name:
+            return "Hello, {}".format(name)
+    return "Who goes there?"
+
+list = ["Peter", "Chris", "Leona"]
+
+print("Please enter your name")
+user_name = input()
+print(name_match(user_name, list))
